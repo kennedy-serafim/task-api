c45dd1545d4c05e896d98a0a48785149992bc73b
Kennedy Serafim <serafimkennedy@gmail.com>
2021-03-16 20:43:48 +0200

    Activity Controller

--- src/controllers/ActivityController.ts
+++ src/controllers/ActivityController.ts
@@ -9,7 +9,7 @@ class ActivityController {
         const { userId } = request.params;
         const activityRepository = getCustomRepository(ActivityRepository);
         const {
-            title, description, start, duration, end
+            title, description, start = new Date(), duration = 1, end, status = 'Pendente'
         } = request.body;
 
         try {
@@ -54,6 +54,213 @@ class ActivityController {
             return response.status(400).json({ error });
         }
     }
+
+    async index(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to get the resource"
+                });
+            }
+
+            const activities = await activityRepository.find({
+                where: {
+                    user
+                },
+                relations: ["activityNotes"]
+            });
+
+            user.password = undefined;
+            return response.json({
+                activities,
+                user
+            });
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
+
+    async findById(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId, id } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to get the resource"
+                });
+            }
+
+            const activities = await activityRepository.findOne(id, {
+                where: {
+                    user
+                },
+                relations: ["activityNotes"]
+            });
+
+            user.password = undefined;
+            return response.json({
+                activities,
+                user
+            });
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
+
+    async findByTitle(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId, title } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to get the resource"
+                });
+            }
+
+            const activities = await activityRepository.findOne({
+                where: {
+                    user,
+                    title
+                },
+                relations: ["activityNotes"]
+            });
+
+            user.password = undefined;
+            return response.json({
+                activities,
+                user
+            });
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
+
+    async findByStatus(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId, status } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to get the resource"
+                });
+            }
+
+            const activities = await activityRepository.findOne({
+                where: {
+                    user,
+                    status
+                },
+                relations: ["activityNotes"]
+            });
+
+            user.password = undefined;
+            return response.json({
+                activities,
+                user
+            });
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
+
+    async update(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId, id } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+        const {
+            title, description, start = new Date(), duration = 1, end, status = 'Pendente'
+        } = request.body;
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to update the resource"
+                });
+            }
+
+            if (!await activityRepository.findOne(id)) {
+                return response.status(404).json({
+                    message: "Activity not found"
+                });
+            }
+
+            if (title == undefined || title.length < 3) {
+                return response.status(400).json({
+                    message: "Title must be longer then 3 caracteres"
+                });
+            }
+
+            await activityRepository.update(id, {
+                title, description, start, duration, end, status
+            });
+
+            return response.status(204).send();
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+
+    }
+
+    async deleteOne(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId, id } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to delete the resource"
+                });
+            }
+
+            const activity = await activityRepository.findOne(id);
+
+            if (!activity) {
+                return response.status(404).json({
+                    message: "Activity not found"
+                });
+            }
+
+            await activityRepository.remove(activity);
+            return response.status(204).send();
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
+
+    async deleteAll(request: Request, response: Response) {
+        const user: any = request.user;
+        const { userId, id } = request.params;
+        const activityRepository = getCustomRepository(ActivityRepository);
+
+        try {
+            if (user.id !== userId) {
+                return response.status(403).json({
+                    message: "Haven't permission to delete the resource"
+                });
+            }
+
+            await activityRepository.delete({
+                user
+            });
+
+            return response.status(204).send();
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
+
+
 }
 
 export { ActivityController };
--- src/routes/ActivityRoutes.ts
+++ src/routes/ActivityRoutes.ts
@@ -6,4 +6,14 @@ const activityController = new ActivityController();
 
 router.post('/activities/users/:userId', activityController.create);
 
+router.put('/activities/:id/users/:userId', activityController.update);
+
+router.delete('/activities/users/:userId', activityController.deleteAll);
+router.delete('/activities/:id/users/:userId', activityController.deleteOne);
+
+router.get('/activities/users/:userId', activityController.index);
+router.get('/activities/:id/users/:userId', activityController.findById);
+router.get('/activities/:title/users/:userId', activityController.findByTitle);
+router.get('/activities/:status/users/:userId', activityController.findByStatus);
+
 export { router };
