dcf57ed78ad16325ee5c462e3d6f90454c03b820
Kennedy Serafim <serafimkennedy@gmail.com>
2021-03-19 19:17:42 +0200

    Setup

--- src/ormconfig.js
+++ src/ormconfig.js
@@ -8,12 +8,11 @@ let extensionDir = process.env.NODE_ENV === "development" ?
 
 module.exports = {
     "type": process.env.TYPE_CONNECTION,
-    "url": "postgres://afxtwmsovdmvxl:4b83ba25d2c9cd19eb20e8c31a4a4f75b6b791e4a817dd8f11143e0f5d2ad8f2@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d3g287tr1brkgd",
-    // "host": process.env.TYPE_HOST,
-    // "port": process.env.TYPE_PORT,
-    // "username": process.env.TYPE_USERNAME,
-    // "password": process.env.TYPE_PASSWORD,
-    // "database": process.env.TYPE_DATABASE,
+    "host": process.env.TYPE_HOST,
+    "port": process.env.TYPE_PORT,
+    "username": process.env.TYPE_USERNAME,
+    "password": process.env.TYPE_PASSWORD,
+    "database": process.env.TYPE_DATABASE,
     "logging": process.env.TYPE_LOGGING,
     "synchronize": process.env.TYPE_SYNCHRONIZE,
 
